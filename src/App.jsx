d1d8fbb2c7c379ca0565433b596d4833ad56a151
Mucyo Elie Joseph <147175412+mucyoelie@users.noreply.github.com>
2026-03-09 16:09:18 +0200

    Add Jazza features

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import WhatsAppButton from './components/WhatsAppButton'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Training from './pages/Training'
+import HireForm from './pages/HireForm'
+import DMEnrollment from './pages/DMEnrollment'
+import Gallery from './pages/Gallery'
+import Contact from './pages/Contact'
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/training" element={<Training />} />
+            <Route path="/hire" element={<HireForm />} />
+            <Route path="/enroll" element={<DMEnrollment />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppButton />
+      </div>
+    </Router>
+  )
+}
